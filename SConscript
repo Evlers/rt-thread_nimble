04e3ac3f3813f7c5a794995a58040448d8a802a6
Evlers <1425295900@qq.com>
2025-02-28 23:08:29 +0800

    first commits

---
+++ SConscript
@@ -0,0 +1,89 @@
+from building import *
+import rtconfig
+
+cwd = GetCurrentDir()
+path = [cwd]
+src = []
+
+path += [
+    cwd + '/nimble/nimble/include',
+    cwd + '/nimble/nimble/host/include',
+    cwd + '/nimble/nimble/host/src',
+    cwd + '/nimble/nimble/host/services/ans/include',
+    cwd + '/nimble/nimble/host/services/bas/include',
+    cwd + '/nimble/nimble/host/services/bleuart/include',
+    cwd + '/nimble/nimble/host/services/gap/include',
+    cwd + '/nimble/nimble/host/services/gatt/include',
+    cwd + '/nimble/nimble/host/services/ias/include',
+    cwd + '/nimble/nimble/host/services/lls/include',
+    cwd + '/nimble/nimble/host/services/tps/include',
+    cwd + '/nimble/nimble/host/store/ram/include',
+    cwd + '/nimble/nimble/host/util/include',
+    cwd + '/nimble/nimble/transport/include',
+    cwd + '/nimble/nimble/transport/common/hci_h4/include',
+    cwd + '/porting/nimble/include',
+    cwd + '/porting/npl/rtthread/include',
+    cwd + '/nimble/ext/tinycrypt/include']
+
+# Host stack
+src += Glob('nimble/nimble/host/src/*.c')
+# src += Glob('nimble/nimble/host/services/*/src/*.c')
+src += Glob('nimble/nimble/host/services/ans/src/*.c')
+src += Glob('nimble/nimble/host/services/bas/src/*.c')
+src += Glob('nimble/nimble/host/services/gap/src/*.c')
+src += Glob('nimble/nimble/host/services/gatt/src/*.c')
+src += Glob('nimble/nimble/host/services/ias/src/*.c')
+src += Glob('nimble/nimble/host/services/lls/src/*.c')
+src += Glob('nimble/nimble/host/services/tps/src/*.c')
+src += Glob('nimble/nimble/host/store/ram/src/*.c')
+src += Glob('nimble/nimble/host/util/src/*.c')
+
+# HCI transport
+src += Glob('nimble/nimble/transport/src/*.c')
+src += Glob('nimble/nimble/transport/common/hci_h4/src/*.c')
+
+# HCI transport porting
+if GetDepend(['RT_NIMBLE_HCI_USING_RTT_UART']) and not GetDepend(['RT_NIMBLE_HCI_USING_CUSTOM_IMPL']):
+    src += ['porting/transport/rtthread/src/ble_hci_rtthread_uart.c']
+if GetDepend(['RT_NIMBLE_HCI_USING_RTT_VHCI']) and not GetDepend(['RT_NIMBLE_HCI_USING_CUSTOM_IMPL']):
+    src += ['porting/transport/rtthread/src/ble_rtthread_vhci_dev.c']
+
+# mesh
+if GetDepend(['RT_NIMBLE_MESH']):
+    path += [cwd + '/nimble/nimble/host/mesh/include']
+    src += Glob('nimble/nimble/host/mesh/src/*.c')
+
+# Few utils and data structures copied from Mynewt
+src += Split('''
+    nimble/porting/nimble/src/endian.c
+    nimble/porting/nimble/src/mem.c
+    nimble/porting/nimble/src/nimble_port.c
+    nimble/porting/nimble/src/os_mbuf.c
+    nimble/porting/nimble/src/os_mempool.c
+    nimble/porting/nimble/src/os_msys_init.c
+    ''')
+
+# npl porting
+src += Glob('porting/npl/rtthread/src/*.c')
+
+# tinycrypt
+src += Glob('nimble/ext/tinycrypt/src/*.c')
+
+
+LOCAL_CCFLAGS = ''
+
+CPPDEFINES = ['']
+
+if rtconfig.CROSS_TOOL == 'keil':
+    LOCAL_CCFLAGS += ' --gnu --diag_suppress=111'
+    # __BYTE_ORDER__ & __ORDER_BIG_ENDIAN__ & __ORDER_LITTLE_ENDIAN__ is not defined in keil, the specific values comes from gcc.
+    CPPDEFINES.append('__ORDER_LITTLE_ENDIAN__=1234')
+    CPPDEFINES.append('__ORDER_BIG_ENDIAN__=4321')
+    CPPDEFINES.append('__BYTE_ORDER__=1234')
+    
+if rtconfig.CROSS_TOOL == 'gcc':
+    LOCAL_CCFLAGS += ' -Wno-format -Wno-unused-variable -Wno-unused-but-set-variable'
+
+group = DefineGroup('nimble', src, depend = ['RT_USING_NIMBLE'], CPPPATH = path, CPPDEFINES = CPPDEFINES, LOCAL_CCFLAGS = LOCAL_CCFLAGS)
+
+Return('group')
---
+++ porting/transport/rtthread/src/ble_hci_rtthread_uart.c
@@ -0,0 +1,204 @@
+/*
+ * Copyright (c) 2006-2025 RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-07-16     Lenoyan      the first version
+ * 2025-01-19     Evlers       update hci h4
+ */
+
+#include <assert.h>
+#include <string.h>
+#include <errno.h>
+#include <stdint.h>
+#include <sysinit/sysinit.h>
+#include <syscfg/syscfg.h>
+#include "host/ble_hs_mbuf.h"
+#include "os/os_mbuf.h"
+#include "nimble/transport.h"
+#include "nimble/transport/hci_h4.h"
+
+#include <rtthread.h>
+#include <rtdevice.h>
+
+#define DBG_TAG             "nimble.vhci"
+#define DBG_LVL             DBG_LOG
+#include "rtdbg.h"
+
+static rt_sem_t rx_sem;
+static struct hci_h4_sm hci_h4sm;
+static rt_device_t hci_dev;
+
+static int hci_uart_frame_cb(uint8_t pkt_type, void *data)
+{
+    switch (pkt_type) {
+    case HCI_H4_EVT:
+        return ble_transport_to_hs_evt(data);
+    case HCI_H4_ACL:
+        return ble_transport_to_hs_acl(data);
+    default:
+        assert(0);
+        break;
+    }
+    return -1;
+}
+
+static rt_err_t rx_indicate(rt_device_t dev, rt_size_t size)
+{
+    rt_sem_release(rx_sem);
+    return RT_EOK;
+}
+
+static void rtthread_hci_rx_entry(void *parameter)
+{
+    uint8_t data[64];
+    size_t data_len;
+
+    rx_sem = rt_sem_create("hci_rx_sem", 0, RT_IPC_FLAG_FIFO);
+    RT_ASSERT(rx_sem != RT_NULL);
+    rt_device_set_rx_indicate(hci_dev, rx_indicate);
+
+    while (1)
+    {
+        rt_sem_take(rx_sem, RT_WAITING_FOREVER);
+        data_len = rt_device_read(hci_dev, 0, data, sizeof(data));
+        if (data_len > 0)
+        {
+            LOG_HEX("Rx: ", 16, data, data_len);
+            hci_h4_sm_rx(&hci_h4sm, data, data_len);
+        }
+    }
+}
+
+static int rtthread_hci_uart_init(void)
+{
+    struct serial_configure config = RT_SERIAL_CONFIG_DEFAULT;
+
+    hci_dev = rt_device_find(RT_NIMBLE_HCI_UART_DEVICE_NAME);
+    if (!hci_dev) {
+        return -1;
+    }
+
+    config.baud_rate = BAUD_RATE_115200;
+    config.data_bits = DATA_BITS_8;
+    config.stop_bits = STOP_BITS_1;
+#ifdef RT_USING_SERIAL_V2
+#else
+    config.bufsz     = 128;
+#endif
+    config.parity    = PARITY_NONE;
+#ifdef RT_NIMBLE_HCI_UART_FLOWCONTROL_CTSRTS
+    config.flowcontrol = RT_SERIAL_FLOWCONTROL_CTSRTS;
+#endif
+
+    rt_device_control(hci_dev, RT_DEVICE_CTRL_CONFIG, &config);
+
+#ifdef RT_USING_SERIAL_V2
+    rt_device_open(hci_dev, RT_DEVICE_FLAG_RX_NON_BLOCKING | RT_DEVICE_FLAG_TX_BLOCKING);
+#else
+    rt_device_open(hci_dev, RT_DEVICE_FLAG_INT_RX);
+#endif
+
+    rt_thread_t rx_thread = rt_thread_create("hci_uart_rx", rtthread_hci_rx_entry, RT_NULL, 1024, 25, 10);
+    if (rx_thread != RT_NULL) {
+        rt_thread_startup(rx_thread);
+    } else {
+        LOG_E("hci rx thread create failed");
+        return -1;
+    }
+
+    return 0;
+}
+
+int ble_transport_to_ll_cmd_impl(void *buf)
+{
+    int res = 0;
+    int buf_len = 3 + ((uint8_t *)buf)[2] + 1;
+    uint8_t *data = NULL;
+
+    data = rt_malloc(buf_len);
+    if (!data) {
+        LOG_E("%s: malloc failed", __func__);
+        res = -RT_ERROR;
+        goto __exit;
+    }
+
+    data[0] = HCI_H4_CMD;
+    memcpy(&data[1], buf, buf_len - 1);
+
+    LOG_HEX("Tx: ", 16, data, buf_len);
+    if (rt_device_write(hci_dev, 0, data, buf_len) != buf_len) {
+        LOG_E("Error writing HCI_H4_CMD data");
+        res = -RT_ERROR;
+        rt_free(data);
+        goto __exit;
+    }
+    rt_free(data);
+
+    __exit:
+    ble_transport_free(buf);
+
+    return res;
+}
+
+int ble_transport_to_ll_acl_impl(struct os_mbuf *om)
+{
+    int res = 0;
+    struct os_mbuf *x = om;
+
+    while (x != NULL)
+    {
+        int data_len = OS_MBUF_PKTLEN(x) + 1;
+        uint8_t *data = NULL;
+
+        LOG_D("ble_transport_to_ll_acl_impl: %d bytes", data_len);
+
+        data = rt_malloc(data_len);
+        if (!data) {
+            LOG_E("%s: malloc failed", __func__);
+            res = -RT_ERROR;
+            goto __exit;
+        }
+
+        data[0] = HCI_H4_ACL;
+        res = ble_hs_mbuf_to_flat(x, &data[1], OS_MBUF_PKTLEN(x), NULL);
+        if (res) {
+            LOG_E("Error copying HCI_H4_ACL data %d", res);
+            res = -RT_ERROR;
+            rt_free(data);
+            goto __exit;
+        }
+
+        LOG_HEX("Tx: ", 16, data, data_len);
+        if (rt_device_write(hci_dev, 0, data, data_len) != data_len) {
+            LOG_E("Error writing HCI_H4_ACL data");
+            res = -RT_ERROR;
+            rt_free(data);
+            goto __exit;
+        }
+        rt_free(data);
+
+        x = SLIST_NEXT(x, om_next);
+    }
+
+    __exit:
+    os_mbuf_free_chain(om);
+
+    return res;
+}
+
+static int rtthread_ble_transport_init(void)
+{
+    int rc;
+    SYSINIT_ASSERT_ACTIVE();
+
+    hci_h4_sm_init(&hci_h4sm, &hci_h4_allocs_from_ll, hci_uart_frame_cb);
+
+    rc = rtthread_hci_uart_init();
+    SYSINIT_PANIC_ASSERT(rc == 0);
+
+    return 0;
+}
+INIT_APP_EXPORT(rtthread_ble_transport_init);
